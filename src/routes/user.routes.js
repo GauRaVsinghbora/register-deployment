844c2db01360f297b40992022fed0d14f4e55bb4
Gaurav singh <gauravbora2273@gmail.com>
2025-01-11 23:40:12 +0530

    fasting the render

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,4 +4,5 @@ import {registerUser} from "../controllers/user.controllers.js"
 const Route = router();
 
 Route.route("/register").post(registerUser);
+
 export default Route;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import connectDB from "./db/index.js";
 import {app} from "./app.js"
+import axios from 'axios'
 
 connectDB()
 .then(()=>{
@@ -14,3 +15,17 @@ connectDB()
     console.log(`there is some error in listing the server`,error);
     process.exit(1)   //immediately ends the current process.
 })
+
+const url ="https://register-deployment.onrender.com/";
+const interval = 30000;
+function reloadWebsite(){
+    axios
+    .get(url)
+    .then((response)=>{
+        console.log("website reloaded");
+    })
+    .catch((error)=>{
+        console.error('error',error.message);
+    });
+}
+sertInterval(reloadWebsite,interval);
