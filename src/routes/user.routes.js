8a4cf82b37b2243960054119af7a62e72ec77a8f
Gaurav singh <gauravbora2273@gmail.com>
2025-01-11 23:59:55 +0530

    adding the / router to reload the website after 30 second for render hoster

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,5 +4,7 @@ import {registerUser} from "../controllers/user.controllers.js"
 const Route = router();
 
 Route.route("/register").post(registerUser);
-
+Route.get("/", (req, res) => {
+    res.status(200).json({ message: "Users API is working!" });
+});
 export default Route;
